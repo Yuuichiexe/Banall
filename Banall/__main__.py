0dd0df63c64ec86d071a3f00632e27d3488a47ab
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2023-11-16 16:43:51 +0530

    Update __main__.py

--- Banall/__main__.py
+++ Banall/__main__.py
@@ -1,10 +1,13 @@
 import os
 import time
-from pyrogram import filters, idle
+from pyrogram import Client, filters, idle
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from datetime import datetime
 from . import bot 
 from Banall import STARTED, FINISH, ERROR, OWN_UNAME
-from datetime import datetime
+
+# Initialize the bot
+bot = Client("my_bot")
 
 # Track bot start time
 start_time = time.time()
@@ -15,40 +18,40 @@ def main(_, msg: Message):
     me = chat.get_member(bot.get_me().id)
     if chat.get_member(msg.from_user.id) and me.can_restrict_members and me.can_delete_messages:
         try:
-            msg.reply(STARTED.format(chat.members_count))
+            msg.reply_text(STARTED.format(chat.members_count))
             count_kicks = 0
             for member in chat.iter_members():
                 if not member.can_manage_chat:
-                    bot.ban_chat_member(chat_id=msg.chat.id, user_id=member.user.id)
+                    bot.kick_chat_member(chat_id=msg.chat.id, user_id=member.user.id)
                     count_kicks += 1
-            msg.reply(FINISH.format(count_kicks))
+            msg.reply_text(FINISH.format(count_kicks))
         except Exception as e:
-            msg.reply(ERROR.format(str(e)))
+            msg.reply_text(ERROR.format(str(e)))
     else:
-        msg.reply("ɪ ɴᴇᴇᴅ ᴛᴏ ʙᴇ ᴀᴅᴍɪɴ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ ᴛᴏ ᴘᴇʀғᴏʀᴍ ᴛʜɪs ᴀᴄᴛɪᴏɴ !")
+        msg.reply_text("ɪ ɴᴇᴇᴅ ᴛᴏ ʙᴇ ᴀᴅᴍɪɴ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ ᴛᴏ ᴘᴇʀғᴏʀᴍ ᴛʜɪs ᴀᴄᴛɪᴏɴ !")
 
 @bot.on_message(filters.group & filters.service, group=2)
-def service(c, m):
-    m.delete()
+def service(_, msg: Message):
+    msg.delete()
 
 @bot.on_message(filters.private & filters.command("start"))
 def start(_, msg: Message):
     msg.reply_photo(
-                    photo="https://te.legra.ph/file/a7aa081d0ce7f3b0124aa.jpg", 
-                    caption="ʜᴇʏ ᴅᴜᴅᴇ, ɪ'ᴍ ᴀɴ ᴀᴅᴠᴀɴᴄᴇ & ᴘᴏᴡᴇʀꜰᴜʟ ʙᴀɴ-ᴀʟʟ ʙᴏᴛ ᴡɪᴛʜ ɴᴏ ʟᴀɢ ɪꜱꜱᴜᴇꜱ. ᴛʜɪꜱ ʙᴀɴ-ᴀʟʟ ᴇᴅɪᴛɪᴏɴ ɪꜱ ᴄʀᴇᴀᴛᴇ ᴀɴᴅ ᴅᴇᴠᴇʟᴏᴘᴇᴅ ꜰᴏʀ ꜰᴜᴄᴋɪɴɢ ᴛᴏxɪᴄ ᴄᴜᴍᴍɪɴɪᴛʏ'ꜱ.\n───────────────────────\nɴᴏᴡ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴇɴᴇᴍʏ'ꜱ ɢʀᴏᴜᴘ ᴀɴᴅ ᴅᴏɴ'ᴛ ғᴏʀɢᴇᴛ ᴛᴏ ɢɪᴠᴇ ᴍᴇ ᴛʜᴇ  ʙᴀɴ ʀɪɢʜᴛꜱ. ᴛʜᴇɴ  sᴇɴᴅ /banall ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ ᴀɴᴅ ɪ ᴡɪʟʟ ꜰᴜᴄᴋ ᴛʜᴇ ᴡʜᴏʟᴇ ɢʀᴏᴜᴘ. \n◎ᴘᴏᴡᴇʀᴇᴅ ʙʏ ˹ʙᴏɴᴛᴇɴ ɴᴇᴛᴡᴏʀᴋ˼", 
-                    reply_markup=InlineKeyboardMarkup(
-                                                      [
-                                                       [
-                                                        InlineKeyboardButton("ꜱᴜᴘᴘᴏʀᴛ", url="https://t.me/bonten_mainchats"), 
-                                                        InlineKeyboardButton("ᴄʜᴀɴɴᴇʟ", url="https://t.me/Bonten_Destroyers")                                      
-                                                       ], 
-                                                       [
-                                                        InlineKeyboardButton("oᴡɴᴇʀ⎋", url=f"https://t.me/{OWN_UNAME}"),
-                                                        InlineKeyboardButton("ꜰᴇᴀᴛᴜʀᴇꜱ", callback_data="help_command")  # Added Help Button
-                                                       ]                                                     
-                                                      ]
-                                                     )
-)
+        photo="https://te.legra.ph/file/a7aa081d0ce7f3b0124aa.jpg",
+        caption="ʜᴇʏ ᴅᴜᴅᴇ, ɪ'ᴍ ᴀɴ ᴀᴅᴠᴀɴᴄᴇ & ᴘᴏᴡᴇʀꜰᴜʟ ʙᴀɴ-ᴀʟʟ ʙᴏᴛ ᴡɪᴛʜ ɴᴏ ʟᴀɢ ɪꜱꜱᴜᴇꜱ. ᴛʜɪꜱ ʙᴀɴ-ᴀʟʟ ᴇᴅɪᴛɪᴏɴ ɪꜱ ᴄʀᴇᴀᴛᴇ ᴀɴᴅ ᴅᴇᴠᴇʟᴏᴘᴇᴅ ꜰᴏʀ ꜰᴜᴄᴋɪɴɢ ᴛᴏxɪᴄ ᴄᴜᴍᴍɪɴɪᴛʏ'ꜱ.\n───────────────────────\nɴᴏᴡ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴇɴᴇᴍʏ'ꜱ ɢʀᴏᴜᴘ ᴀɴᴅ ᴅᴏɴ'ᴛ ғᴏʀɢᴇᴛ ᴛᴏ ɢɪᴠᴇ ᴍᴇ ᴛʜᴇ  ʙᴀɴ ʀɪɢʜᴛꜱ. ᴛʜᴇɴ sᴇɴᴅ /banall ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ ᴀɴᴅ ɪ ᴡɪʟʟ ꜰᴜᴄᴋ ᴛʜᴇ ᴡʜᴏʟᴇ ɢʀᴏᴜᴘ. \n◎ᴘᴏᴡᴇʀᴇᴅ ʙʏ ˹ʙᴏɴᴛᴇɴ ɴᴇᴛᴡᴏʀᴋ˼",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("ꜱᴜᴘᴘᴏʀᴛ", url="https://t.me/bonten_mainchats"),
+                    InlineKeyboardButton("ᴄʜᴀɴɴᴇʟ", url="https://t.me/Bonten_Destroyers")
+                ],
+                [
+                    InlineKeyboardButton("oᴡɴᴇʀ⎋", url=f"https://t.me/{OWN_UNAME}"),
+                    InlineKeyboardButton("ꜰᴇᴀᴛᴜʀᴇꜱ", callback_data="help_command")  # Added Help Button
+                ]
+            ]
+        )
+    )
 
 @bot.on_callback_query(filters.regex("help_command"))
 def help_command_handler(_, callback_query):
@@ -56,13 +59,13 @@ def help_command_handler(_, callback_query):
     callback_query.message.delete()
     bot.send_message(
         callback_query.from_user.id,
-        "ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅꜱ ꜰᴏʀ ʙᴀɴ-ᴀʟʟ ʙᴏᴛ:\n\n"
+        "ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅꜱ ꜰᴏʀ ʙᴀɴ-ᴀʟʟ ʙᴏᴛ⥮\n\n"
         "───────────────────────\n"
-        "/banall ⥬ ʙᴀɴᴀʟʟ ᴀʟʟ ᴛʜᴇ ᴍᴇᴍʙᴇʀꜱ ꜰʀᴏᴍ ᴛʜᴇ ɢʀᴏᴜᴘ.\n"
-        "/start ⥬ ᴊᴜꜱᴛ ꜱɪᴍᴘʟᴇ ᴄᴍᴅ ᴛᴏ ꜱᴛᴀʀᴛ ʙᴏᴛ ᴛʜᴇ ʙᴏᴛ .\n"
+        "/banall ⥬ ʙᴀɴ ᴀʟʟ ᴛʜᴇ ᴍᴇᴍʙᴇʀꜱ ꜰʀᴏᴍ ɢʀᴏᴜᴘ.\n"
+        "/start ⥬ ᴄᴏᴍᴍᴀɴᴅ ᴛᴏ ꜱᴛᴀʀᴛ ʙᴏᴛ ᴛʜᴇ ʙᴏᴛ.\n"
         "/ping ⥬ ᴄʜᴇᴄᴋ ᴀɪ'ꜱ ᴜᴘᴛɪᴍᴇ ᴀɴᴅ ꜱᴘᴇᴇᴅ.\n"
         "───────────────────────\n\n"
-        "ꜰᴏʀ ꜰᴜʀᴛʜᴇʀ ᴀꜱꜱɪꜱᴛᴀɴᴄᴇ ᴊᴏɪɴ ᴏᴜʀ [ꜱᴜᴘᴘᴏʀᴛ](https://t.me/bonten_mainchats) \n"
+        "ꜰᴏʀ ꜰᴜʀᴛʜᴇʀ ᴀꜱꜱɪꜱᴛᴀɴᴄᴇ ᴊᴏɪɴ ᴏᴜʀ [ꜱᴜᴘᴘᴏʀᴛ](https://t.me/bonten_mainchats) \n\n"
         "ᴀʟꜱᴏ ᴄʜᴇᴄᴋ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ [ᴄʜᴀɴɴᴇʟ](https://t.me/Bonten_Destroyers).",
         disable_web_page_preview=True
     )
@@ -72,11 +75,12 @@ def ping(_, msg: Message):
     uptime = round(time.time() - start_time)
     start = datetime.now()
     end = datetime.now()
-    ms = (end-start).microseconds / 1000
-    msg.reply(f"🏓 **Pong!**\n\nUptime: `{uptime // 60}m {uptime % 60}s`\nPing: `{ms}ms`")
+    ms = (end - start).microseconds / 1000
+    msg.reply_text(f"🏓 **𝙿ᴏɴɢ!**\n\n────────────────────\nᏴคɴᴀʟʟ 𝙱ᴏᴛ [⚡️] ɪꜱ ʀᴇᴀᴅʏ ᴛᴏ ꜰᴜᴄᴋ ᴛᴏxɪᴄ ɢᴄ'ꜱ\n👾 ᴜᴘᴛɪᴍᴇ ᴏꜰ ᴛʜᴇ ʙᴏᴛ: `{uptime // 60}m {uptime % 60}s`\n💥 ꜱᴘᴇᴇᴅ ᴏꜰ ʙᴀɴ-ᴀʟʟ ᴀɪ: `{ms}ms`")
 
 bot.run()
 idle()
 
-print("ᴅᴏɴᴇ ʙᴀɴᴀʟʟ  sᴛᴀʀᴛᴇᴅ ...") 
+print("ᴅᴏɴᴇ ʙᴀɴᴀʟʟ  sᴛᴀʀᴛᴇᴅ ...")
 print("ᴊᴏɪɴ  @bonten_mainchats ꜰᴏʀ ʜᴇʟᴘ")
+      
