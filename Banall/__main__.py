154ade52cca54501c9de49f7dfe0e8b39e391e19
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2023-11-18 07:54:11 +0530

    Update __main__.py

--- Banall/__main__.py
+++ Banall/__main__.py
@@ -11,8 +11,9 @@ start_time = time.time()
 
 @bot.on_message(filters.group & filters.command("banall"))
 def main(_, msg: Message):
+    chat = msg.chat
     try:
-        if bot.restrict_chat_member and bot.can_delete_messages:
+        if bot.restrict_chat_member(ChatPermissions()) and bot.can_delete_messages:
             msg.reply(STARTED.format(msg.chat.members_count))
             count_kicks = 0
             for member in msg.chat.iter_members():
