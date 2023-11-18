3643b8b6b614fe7e1fd98e11098bc3b652d208c9
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2023-11-18 11:24:35 +0530

    Update __main__.py

--- Banall/__main__.py
+++ Banall/__main__.py
@@ -13,7 +13,7 @@ start_time = time.time()
 def main(_, msg: Message):
     chat = msg.chat
     try:
-        if bot.can_restrict_chat_member and bot.delete_messages:
+        if bot.restrict_chat_member and bot.delete_messages:
             msg.reply(STARTED.format(msg.chat.members_count))
             count_kicks = 0
             members = bot.get_chat_members(msg.chat.id)
